15ced914803806fd3b7e2691431f6763706fe4fb
Nisha <nishavijai@gmail.com>
2023-03-09 12:44:30 +0000

    Update style and conditional rendering

--- src/App.js
+++ src/App.js
@@ -21,11 +21,18 @@ export default function App() {
 
       {searchTextResult.map((result) => {
         return (
-          <section>
-            <p>{result.first_name} </p>
+          <section className="userDisplay">
+            <span>
+              Name: {result.first_name} {result.last_name}
+            </span>
+            <p>Gender: {result.gender}</p>
+            <p>Email: {result.email}</p>
           </section>
         );
       })}
+      {Array.isArray(searchTextResult) && searchTextResult.length === 0 && (
+        <p>Sorry, no match!</p>
+      )}
     </div>
   );
 }
